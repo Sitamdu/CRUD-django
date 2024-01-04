0acff385a26330dbbb62d02627366319edc92c84
Sitamdu <Beekramrai22@gmail.com>
2024-01-04 11:18:37 +0545

    htmlupdate

--- home/models.py
+++ home/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+#Create your models here.
 class Detail(models.Model):
     firstname = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
